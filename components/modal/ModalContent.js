83bc7d0be186e89d1374d5e9599e18d2f8fd1ba5
venkatapathy <venkatapathy@billiontags.com>
2020-04-17 17:21:29 +0530

    overlay click fix

--- components/modal/ModalContent.js
+++ components/modal/ModalContent.js
@@ -1,11 +1,15 @@
-import React from "react"
+import React from "react";
 
 const ModalContent = ({ children, className, style = {} }) => {
     return (
-        <div className={className} style={style}>
+        <div
+            className={className}
+            onClick={(e) => e.stopPropagation()}
+            style={style}
+        >
             {children}
         </div>
-    )
-}
+    );
+};
 
-export default ModalContent
+export default ModalContent;
