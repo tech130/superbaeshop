078e8604a3dd9a718e2783b7c09d7fecbb81a6ab
venkatapathy <venkatapathy@billiontags.com>
2020-04-18 16:30:44 +0530

    checkout page

---
+++ components/icons/ThrashIcon.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function ThrashIcon({ size = 24, ...props }) {
+    return (
+        <svg
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="prefix__feather prefix__feather-trash-2"
+            {...props}
+        >
+            <path d="M3 6h18M19 6v14a2 2 0 01-2 2H7a2 2 0 01-2-2V6m3 0V4a2 2 0 012-2h4a2 2 0 012 2v2M10 11v6M14 11v6" />
+        </svg>
+    );
+}
+
+export default ThrashIcon;
--- pages/checkout.js
+++ pages/checkout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Layout from "../components/layout/Layout";
+import ThrashIcon from "../components/icons/ThrashIcon";
 
 const Checkout = () => {
     return (
@@ -8,6 +9,7 @@ const Checkout = () => {
                 <div className="container">
                     <div className="row">
                         <div className="col-lg-7">
+                            <CheckoutSummary />
                             <CartItem />
                             <CartItem />
                             <CartItem />
@@ -155,7 +157,7 @@ const Checkout = () => {
                                 <div className="col-md-12">
                                     <div className="form-group">
                                         <button className="btn btn-submit">
-                                            Checkout
+                                            PAY NOW
                                         </button>
                                     </div>
                                 </div>
@@ -168,28 +170,117 @@ const Checkout = () => {
     );
 };
 
+const CheckoutSummary = () => {
+    return (
+        <>
+            <div className="checkout-summary mb-3">
+                <div className="d-flex align-items-center justify-content-between mb-2">
+                    <h6 className="font-weight-bold m-0">
+                        Cart Total
+                        <span className="ml-1 font-weight-light">
+                            (<span>1</span> X <span>₹1536</span>)
+                        </span>
+                    </h6>
+                    <div>₹1536</div>
+                </div>
+                <div className="d-flex align-items-center justify-content-between mb-2">
+                    <h6 className="font-weight-bold m-0">
+                        Delivery Charge
+                        <span className="ml-1 font-weight-light">
+                            (<span>1</span> X <span>₹0</span>)
+                        </span>
+                    </h6>
+                    <div>+ ₹0</div>
+                </div>
+                <hr className="checkout-summary-divider mb-1 mt-2" />
+                <div className="d-flex align-items-center justify-content-between mb-2">
+                    <h6 className="font-weight-bold m-0">Grand Total</h6>
+                    <div className="font-weight-bold">₹1536</div>
+                </div>
+            </div>
+            <style jsx>{`
+                .checkout-summary {
+                    padding: 10px;
+                    border: 2px solid #f5f5f5;
+                    border-radius: 10px;
+                }
+                .checkout-summary-divider {
+                    border-top: 1px solid #f5f5f5;
+                }
+            `}</style>
+        </>
+    );
+};
+
 const CartItem = () => {
     return (
         <>
-            <div className="cart-item d-flex mb-3">
+            <div className="cart-item d-flex align-items-stretch mb-3">
                 <img
                     className="cart-img"
                     src="https://letsgoal2020.com/static/media/front.e7158c6a.png"
                     alt=""
                 />
-                <div>
-                    <h5 className="mb-1">Let's Goal 2021</h5>
+                <div className="flex-grow-1">
+                    <h5 className="cart-product-title mb-1">Let's Goal 2021</h5>
+                    <div className="mb-2">
+                        <span className="cart-original-price mr-1">₹500 </span>
+                        <span className="cart-offer-price"> ₹600 </span>
+                        <span> (40% off)</span>
+                    </div>
+                    <div className="d-flex align-items-center">
+                        <button className="btn cart-qty-btn">-</button>
+                        <div className="cart-quantity">1</div>
+                        <button className="btn cart-qty-btn">+</button>
+                    </div>
+                </div>
+                <div className="d-flex justify-content-center align-items-center pl-2 pr-2">
+                    <button className="btn">
+                        <ThrashIcon size={18} strokeWidth={1} />
+                    </button>
                 </div>
             </div>
             <style jsx>{`
                 .cart-item {
-                    border: 1px solid var(--theme-border);
-                    padding: 20px;
+                    padding: 10px;
+                    border: solid 2px #f5f5f5;
+                    border-radius: 10px;
                 }
                 .cart-img {
-                    width: 150px;
+                    width: 100px;
                     margin-right: 15px;
                     height: auto;
+                }
+                .cart-product-title {
+                    font-size: 16px;
+                    font-weight: 500;
+                }
+                .cart-offer-price {
+                    font-weight: 600;
+                    font-size: 16px;
+                }
+                .cart-original-price {
+                    font-weight: 300;
+                    text-decoration: line-through;
+                    font-size: 14px;
+                }
+                .cart-qty-btn {
+                    height: 25px;
+                    width: 25px;
+                    border-radius: 50%;
+                    text-align: center;
+                    border: 1px solid #cecece;
+                    font-weight: 600;
+                    line-height: 0;
+                }
+                .cart-quantity {
+                    height: 25px;
+                    width: 40px;
+                    text-align: center;
+                    border: 1px solid #cecece;
+                    font-weight: 500;
+                    margin: 0px 10px;
+                    font-size: 14px;
                 }
             `}</style>
         </>
