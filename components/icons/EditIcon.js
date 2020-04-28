a0f1959d7114cee14f7fee2205f361bc0d39f115
venkatapathy <venkatapathy@billiontags.com>
2020-04-28 16:55:39 +0530

    custom select init

---
+++ components/icons/EditIcon.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function EditIcon({ size = 24, props }) {
+    return (
+        <svg
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth={2}
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            {...props}
+        >
+            <path d="M17 3a2.828 2.828 0 114 4L7.5 20.5 2 22l1.5-5.5L17 3z" />
+        </svg>
+    );
+}
+
+export default EditIcon;
