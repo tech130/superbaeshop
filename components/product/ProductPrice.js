ba015145d246c000e6db5bb3a0b992e5a36e88bc
venkatapathy <venkatapathy@billiontags.com>
2020-04-30 22:50:12 +0530

    product price

---
+++ components/product/ProductPrice.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import Txt from "../styled/Txt";
+import { useProdCountry } from "../common/CountryLink";
+
+const ProductPrice = ({ id, ...rest }) => {
+    const product = useSelector((state) => state.product[id] || {});
+
+    if (product.product_country) {
+        return (
+            <ProdPrice product_country={product.product_country} {...rest} />
+        );
+    }
+    return null;
+};
+
+const ProdPrice = ({
+    product_country = [],
+    type = "selling_price",
+    quantity = 1,
+    ...rest
+}) => {
+    const productCountry = useProdCountry(product_country);
+
+    if (productCountry && productCountry.country) {
+        return (
+            <Txt
+                textDecor={type === "original_price" ? "line-through" : "none"}
+                {...rest}
+            >
+                {productCountry.country.currency_type}
+                {quantity * (productCountry[type] || 0)}
+            </Txt>
+        );
+    }
+
+    return null;
+};
+
+export default ProductPrice;
