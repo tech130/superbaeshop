3b469e87d39ef7dbe2701a3464b976ae9e32d57f
venkatapathy <venkatapathy@billiontags.com>
2020-07-31 12:43:27 +0530

    fetch profile

--- components/profile/MyProfile.js
+++ components/profile/MyProfile.js
@@ -1,19 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Wallet from "./Wallet";
-import WalletItem from "./WalletItem";
 import useUser from "../../hooks/redux/user/useUser";
 import { Col, Row, Container } from "styled-bootstrap-grid";
 import Block from "../styled/Block";
 import ProfileDetails from "./ProfileDetails";
 import { H6 } from "../styled/Headings";
 import WalletList from "./WalletList";
+import { useDispatch } from "react-redux";
+import { fetchProfile } from "../../redux/user/user";
 
 const MyProfile = () => {
+    const dispatch = useDispatch();
     const user = useUser();
+    const { token } = user;
+
+    useEffect(() => {
+        if (token) {
+            dispatch(fetchProfile());
+        }
+    }, [token]);
+
     return (
         <Block padding="35px 0px">
             <Container>
-                {user.token ? (
+                {token ? (
                     <Row>
                         <Col lg={6}>
                             <ProfileDetails {...user} />
