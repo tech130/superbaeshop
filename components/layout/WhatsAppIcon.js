f5edd3099591c02f5b67d52290ea1fd41342d367
venkatapathy <venkatapathy@billiontags.com>
2020-08-03 20:36:09 +0530

    9600999559

--- components/layout/WhatsAppIcon.js
+++ components/layout/WhatsAppIcon.js
@@ -23,7 +23,7 @@ const WhatsAppIcon = () => {
         <Portal id="whatsapp-icon">
             <WhatsAppLink
                 className="whatsIcon"
-                href="https://wa.me/919787054444"
+                href="https://wa.me/919600999559"
                 target="_blank"
                 rel="noopener noreferrer"
             >
