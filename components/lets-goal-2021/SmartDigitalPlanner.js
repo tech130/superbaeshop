43280842536fb153e232c4a893a68cedd81f0b43
Jeba Godson <jeba@accubits.com>
2021-10-05 10:35:35 +0530

    build failed

--- components/lets-goal-2021/SmartDigitalPlanner.js
+++ components/lets-goal-2021/SmartDigitalPlanner.js
@@ -166,24 +166,8 @@ const Position = styled.div`
 
 const SmartDigitalPlanner = () => {
     // const domRef = useRef();
-    const [isVisible, setVisible] = React.useState(false);
-    useEffect(() => {
-        // const observer = new IntersectionObserver(entries => {
-        //     // In your case there's only one element to observe:     
-        //     console.log(entries)
-        //     if (entries[0].isIntersecting) {
-        //         // Not possible to set it back to false like this:
-        //         setVisible(true);
-
-        //         // No need to keep observing:
-        //         observer.unobserve(domRef.current);
-        //     }
-        // });
-
-        // observer.observe(domRef.current);
-
-        // return () => observer.unobserve(domRef.current);
-    }, []);
+    // const [isVisible, setVisible] = React.useState(false);
+    
     return (
         <Block padding="30px 0px" className="overflow-hidden">
             <StyledDiv>
