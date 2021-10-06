e76838a03d24db651fe16e91cf8c254d215a5861
Jeba Godson <jeba@accubits.com>
2021-10-06 21:53:13 +0530

    Affiliates page check

---
+++ pages/[country]/affiliates.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../../components/layout/Layout";
+import Block from "../../components/styled/Block";
+
+const Affiliates = () => {
+    return (
+        <Layout>
+            <Block padding="35px 0px">
+                Check
+            </Block>
+        </Layout>
+    );
+};
+
+// Terms.getInitialProps = async (ctx) => {
+//     await common(ctx);
+//     await ctx.store.dispatch(fetchTerms());
+// };
+
+export default Affiliates;
