bdd9306106fbdf76444a828df04dbcf9a18d97f8
Jeba Godson <jeba@accubits.com>
2021-10-22 22:47:15 +0530

    change api base url into live url

--- apiInstance.js
+++ apiInstance.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 const { CancelToken, create, isCancel } = axios;
 
-// const baseURL = "https://spaceandbeauty.billioncart.in/";
-const baseURL = "https://spaceandbeauty.billiontags.com/";
+// const baseURL = "https://spaceandbeauty.billiontags.com/";
+const baseURL = "https://api.spaceandbeauty.com/";
 
 const apiInstance = create({
     baseURL,
