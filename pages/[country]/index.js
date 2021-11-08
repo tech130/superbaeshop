a80d44ec1d6b0f1a91658fd85ededf18007c8ddb
Jeganeswaran <jegatheeshwaran@billiontags.com>
2021-11-08 14:21:24 +0530

    landing page changes

--- pages/[country]/index.js
+++ pages/[country]/index.js
@@ -1,7 +1,4 @@
-import ProPlanner from "../../components/professional-planner/ProPlanner";
 import LetsGoal2021 from "../../components/lets-goal-2021/LetsGoal2021";
 import ProductPage from "../../components/product/ProductPage";
-import slug from "../../components/professional-planner/slug";
-import HomePage from "../../components/homepage/homepage";
 
-export default ProductPage(slug, HomePage);
+export default ProductPage("lets-goal-2021-planner", LetsGoal2021);
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,4 @@
-import ProPlanner from "../components/professional-planner/ProPlanner";
 import ProductPage from "../components/product/ProductPage";
-import slug from "../components/professional-planner/slug";
-import HomePage from "../components/homepage/homepage";
+import LetsGoal2021 from "../components/lets-goal-2021/LetsGoal2021";
 
-export default ProductPage(slug, HomePage);
+export default ProductPage("lets-goal-2021-planner", LetsGoal2021);
