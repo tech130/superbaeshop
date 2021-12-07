c5b8eb5abf45a733172b0604aa0a3d3ac750844b
Jeganeswaran <jegatheeshwaran@billiontags.com>
2021-12-07 14:25:53 +0530

    suport number changes done

--- components/layout/WhatsAppIcon.js
+++ components/layout/WhatsAppIcon.js
@@ -21,7 +21,7 @@ const WhatsAppLink = styled.a`
 const WhatsAppIcon = ({ slug = "" }) => {
     const product = useProduct(slug);
     const number =
-        product && product.support_number ? product.support_number : "919600999559";
+        product && product.support_number ? product.support_number : "9791311511";
 
     return (
         <Portal id="whatsapp-icon">
